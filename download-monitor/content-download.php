04d1ec7d3a2c6f87709ee1b2013dbe84afba432f
Rosa Braatz <rosa.braatz@outlook.de>
2018-11-28 12:58:16 +0100

    add styling for download page; same textcolor (black vs. 333)

---
+++ download-monitor/content-download.php
@@ -0,0 +1,4 @@
+<a class="download-link filetype-icon <?php echo 'filetype-' . $dlm_download->get_version()->get_filetype(); ?>"
+   href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow">
+	<?php $dlm_download->the_title(); ?>
+</a>
