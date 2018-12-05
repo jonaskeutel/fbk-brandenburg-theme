cce9f715988b9ba0d551162ba01153cd9c5de5e2
Rosa Braatz <rosa.braatz@outlook.de>
2018-12-05 12:26:41 +0100

    change some stuff accordingly to some wordpress guidelines

--- header.php
+++ header.php
@@ -11,7 +11,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link rel="stylesheet" href= <?php bloginfo('stylesheet_url') ?> type="text/css">
-    <title> <?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
   </head>
 
