796715145ea647b4b098493476c9ed0b9b258f02
Rosa Braatz <rosa.braatz@outlook.de>
2019-01-05 12:14:15 +0100

    refactor js code and add css for images

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 $(function () {
   $("#landing-page-extent").on("click", function() {
-    document.getElementById("landing-page-hidden-text").style.display = "block";
-    document.getElementById("landing-page-extent").style.display = "none";
+    $("#landing-page-hidden-text").slideDown();
+    $("#landing-page-extent").hide();
+    $("#landing-page-hide").show();
+  })
+
+  $("#landing-page-hide").on("click", function() {
+    $("#landing-page-hidden-text").slideUp();
+    $("#landing-page-extent").show();
+    $("#landing-page-hide").hide();
   })
 })
