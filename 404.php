79382e0e14e201f03f78bb7e9237e2ea5d4b65e0
Rosa Braatz <rosa.braatz@outlook.de>
2019-02-19 18:40:25 +0100

    add 404.php

---
+++ 404.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+<div class="content container-fluid">
+  <div class="row">
+    <div class="col-md-1"></div>
+    <div class="col-md-10">
+      <div class="standard-page-title">
+        <h2> Entschuldigung! </h2>
+      </div>
+      <div class="standard-page-content">
+        Diese Seite können wir leider nicht finden. Da ist wohl etwas schief gegangen. Wir bitten um Entschuldigung.
+        <br>
+        <a href="<?php echo esc_url( home_url('/') ); ?>" class="unvisible-link">Hier geht es zurück zur Startseite.</a>
+        <br>
+        Gerne können Sie uns auch schreiben und von dem Problem berichten. <a href="<?php echo esc_url(home_url('/kontakt')); ?>" class="unvisible-link">Hier geht es zum Kontaktformular.</a>
+        <br>
+        Vielen Dank für Ihre Hilfe und ihr Verständnis!
+      </div>
+    </div>
+    <div class="col-md-1"></div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
