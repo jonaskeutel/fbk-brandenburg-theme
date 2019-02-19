773275ce3f3076ed3e6744069a2fa59439e824b9
Rosa Braatz <rosa.braatz@outlook.de>
2019-02-19 18:59:51 +0100

    delete unnecessary folder for no longer used plugin

--- download-monitor/content-download.php
+++
@@ -1,4 +0,0 @@
-<a class="download-link filetype-icon <?php echo 'filetype-' . $dlm_download->get_version()->get_filetype(); ?>"
-   href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow">
-	<?php $dlm_download->the_title(); ?>
-</a>
